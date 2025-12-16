51770aa140d870dd527dc96b6b20d3acfcd9787c
zhumanyao <zhumanyao@sungrowpower.com>
2025-12-16 18:04:11 +0800

    fix: 修改

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 
 # 如果部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@gitee.com:stylepicasso/ocean-of-knowledge.git master:dist
+git push -f git@github.com:meilSop/ocean-of-knowledge.git master:dist
 
 # 如果是部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
